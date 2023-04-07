81e82594ef949beba114c43f7b2cbcaed5fb001a
danielluis07 <bombdaniel@hotmail.com>
2023-04-06 21:46:28 -0300

    first commit

---
+++ client/src/components/CartItem.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { IoClose } from "react-icons/io5";
+import Qty from "../components/Qty";
+import { CartContext } from "../context/CartContext";
+
+const CartItem = ({ item }) => {
+  const { removeFromCart } = useContext(CartContext);
+  return (
+    <div className="flex gap-x-8">
+      <Link to={`product/${item.id}`} className="w-[70px] h-[70px]">
+        <img
+          src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
+          alt="camera"
+        />
+      </Link>
+      <div className="flex-1">
+        {/* title and remove icon */}
+        <div className="flex gap-x-4 mb-3">
+          <Link to={`product/${item.id}`}>{item.attributes.title}</Link>
+          <div
+            onClick={() => removeFromCart(item.id)}
+            className="cursos-pointer text-[24px] hover:text-accent transition-all">
+            <IoClose />
+          </div>
+        </div>
+        <div className="flex items-center gap-x-12">
+          {/* quantity */}
+          <div className="flex gap-x-4 mb-2">
+            <Qty item={item} />
+          </div>
+          <div className="text-accent text-xl">
+            R$ {item.attributes.price * item.amount}
+          </div>
+        </div>
+        {/* price */}
+        <div>
+          <span className="text-accent">
+            R$ {item.attributes.price} por item
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+import LatestProducts from "../components/LatestProducts";
+
+const Home = () => {
+  return (
+    <section>
+      <Hero />
+      <LatestProducts />
+    </section>
+  );
+};
+
+export default Home;
